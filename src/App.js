5e934a96a6c1f0118dafea420fa8970b2315f0b8
Cameron Morgan <cameronmorgan@Camerons-MacBook-Air.local>
2023-03-02 15:39:54 -0800

    Completed the 'Set Up Routes' tasks.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie'
+
+import { Routes, Route } from 'react-router-dom';
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -14,6 +18,8 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movies' slice of state
+          // console.log(response.data);
+          setMovies(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -27,10 +33,15 @@ export default function App () {
   };
 
   return (
+    <>
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+      <Routes>
+        <Route path="/" element={ <MovieList movies={movies} /> } />
+        <Route path="/movies/:id" element={ <Movie />} />
+      </Routes>
     </div>
+
+    </>
   );
 }
